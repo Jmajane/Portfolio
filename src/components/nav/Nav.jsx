198271664409304034e531bab54abd1460e36784
Jmajane <jordan.majane@outlook.com>
2022-06-23 11:36:26 -0400

    css and nav bar setup started

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {FaUserAlt} from 'react-icons/fa'
+import {FaBook} from 'react-icons/fa'
+import {RiServiceFill} from 'react-icons/ri'
+import {BiMessageAltEdit} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><FaHome /></a>
+      <a href="#about"><FaUserAlt /></a>
+      <a href="#experience"><FaBook /></a>
+      <a href="#services"><RiServiceFill /></a>
+      <a href="#contact"><BiMessageAltEdit /></a>
+    </nav>
   )
 }
 
