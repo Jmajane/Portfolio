75cbb0a8d0b41c4a981e5ba9e447114340d5b469
Jmajane <jordan.majane@outlook.com>
2022-06-23 16:44:11 -0400

    about section updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './about.css'
+import USMC from '../../assets/usmc.JPG'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderActive} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={USMC} alt="Marine Corps Picture" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>6+ Months</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Languages</h5>
+              <small>12+ Coding Languages</small>
+            </article>
+            
+            <article className='about__card'>
+              <VscFolderActive className='about__icon' />
+              <h5>Projects</h5>
+              <small>6 Completed Projects</small>
+            </article>
+
+          </div>
+          <p>
+            A Marine Corps Veteran turned into a Software Developer. Someone who
+            enjoys puzzles and troubleshooting almost as much as gaming. Always 
+            excited to learn to add to my toolbox of knowledge. Sometimes you
+            need someone in your corner who wont back down. 
+          </p>
+          <a href="contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './nav.css'
 import {FaHome} from 'react-icons/fa'
-import {FaUserAlt} from 'react-icons/fa'
+import {IoLogoGameControllerB} from 'react-icons/io'
 import {FaBook} from 'react-icons/fa'
 import {RiServiceFill} from 'react-icons/ri'
 import {BiMessageAltEdit} from 'react-icons/bi'
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><IoLogoGameControllerB /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageAltEdit /></a>
