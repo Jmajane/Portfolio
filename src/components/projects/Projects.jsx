e16fe852e1d613b8f2ce01bb6d3d679b298ad76a
Jmajane <jordan.majane@outlook.com>
2022-06-24 14:41:12 -0400

    projects updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,49 +5,63 @@ import STREAMDREAM from '../../assets/streamdream.png'
 import DISNEY from '../../assets/disney.png'
 import NOMS from '../../assets/noms.png'
 
+const data = [
+  {
+    id: 1,
+    image: WAR,
+    title: 'Card Game War',
+    github: 'https://github.com/Jmajane/Card_Game_War',
+    demo: 'https://jmajane.github.io/Card_Game_War/',
+  },
+  {
+    id: 2,
+    image: STREAMDREAM,
+    title: 'StreamDream',
+    github: 'https://github.com/Jmajane/streamDream',
+    demo: 'https://streamdreamproject.herokuapp.com/streamDream',
+  },
+  {
+    id: 3,
+    image: DISNEY,
+    title: 'Disney Characters',
+    github: 'https://github.com/Jmajane/Project_3_Disney_Characters',
+    demo: 'https://disneycharacter.netlify.app/',
+  },
+  {
+    id: 4,
+    image: NOMS,
+    title: 'Noms Food Tracker',
+    github: 'https://github.com/Jmajane/Noms',
+    demo: 'https://noms-capstone.herokuapp.com/about/',
+  },
+]
+
+
+
 const Projects = () => {
   return (
     <section id='projects'>
       <h5>View My Recent Projects</h5>
       <h2>Projects</h2>
 
-      <div className='container portfolio__container'>
+      <div className='container project__container'>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={WAR} alt="Card Game War" />
+        {
+          data.map(({id, image, title, github, demo}) =>{
+            return (
+            <article key={id} className='project__item'>
+          <div className='project__item-image'>
+            <img src={image} alt={title} />
           </div>
-          <h3>Card Game War</h3>
-          <a href="https://github.com/Jmajane/Card_Game_War" className='btn' target='_blank'>GitHub</a>
-          <a href="https://jmajane.github.io/Card_Game_War/" className='btn btn-primary' target='_blank'>LiveDemo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={STREAMDREAM} alt="StreamDream" />
+          <h3>{title}</h3>
+          <div className='project__item-cta'>
+            <a href={github} className='btn' target='_blank'>GitHub</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>LiveDemo</a>
           </div>
-          <h3>StreamDream</h3>
-          <a href="https://github.com/Jmajane/streamDream" className='btn' target='_blank'>GitHub</a>
-          <a href="https://streamdreamproject.herokuapp.com/streamDream" className='btn btn-primary' target='_blank'>LiveDemo</a>
         </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={DISNEY} alt="Disney Characters" />
-          </div>
-          <h3>Disney Characters</h3>
-          <a href="https://github.com/Jmajane/Project_3_Disney_Characters" className='btn' target='_blank'>GitHub</a>
-          <a href="https://disneycharacter.netlify.app/" className='btn btn-primary' target='_blank'>LiveDemo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={NOMS} alt="Noms" />
-          </div>
-          <h3>Noms</h3>
-          <a href="https://github.com/Jmajane/Noms" className='btn' target='_blank'>GitHub</a>
-          <a href="https://noms-capstone.herokuapp.com/about/" className='btn btn-primary' target='_blank'>LiveDemo</a>
-        </article>
+            )
+          })
+        }
 
       </div>
     </section>
