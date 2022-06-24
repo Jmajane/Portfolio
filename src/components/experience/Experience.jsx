5f50718bbd95fbbd3fc682166c23e1f54131a627
Jmajane <jordan.majane@outlook.com>
2022-06-24 10:37:59 -0400

    experience section updated

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,44 +13,90 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>BootStrap</h4>
-              <small className='text-light'>Intermediate</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>BootStrap</h4>
+                <small className='text-light'>Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>Django</h4>
-              <small className='text-light'>Intermediate</small>
-            </article>
-            <article className="experience__details">
-              <FaFlagCheckered />
-              <h4>Bulma</h4>
-              <small className='text-light'>Beginner</small>
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>Bulma</h4>
+                <small className='text-light'>Basic</small>
+              </div>
             </article>
           </div>
         </div>
-        <div className="experience__backend">
 
+
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>MongoDb</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>PostgresSQL</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaFlagCheckered className="experience__details-icon" />
+              <div>
+                <h4>Django</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
     </section>
