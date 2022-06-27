4877cdbad3ad64077b2a19ca1558d217bf4ec249
Jmajane <jordan.majane@outlook.com>
2022-06-27 10:28:12 -0400

    testimonials updated

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar.webp'
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+const reviews = [
+  {
+    avatar: AVTR1,
+    name: "Joshua Smith",
+    review: "Blah Blah Blah",
+  },
+  {
+    avatar: AVTR1,
+    name: "Michael Howey",
+    review: "Blah Blah Blah",
+  },
+  {
+    avatar: AVTR1,
+    name: "Brian Krabec",
+    review: "Blah Blah Blah",
+  },
+]
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Reviews from</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className='container testimonials__container'
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          reviews.map(({avatar, name, review}, index) => {
+            return (
+            <SwiperSlide key={index} className='testimonial'>
+            <div className='client__avatar'>
+              <img src={avatar} alt={name} />
+            </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>{review}</small>
+        </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
